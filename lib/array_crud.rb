6007df4a5b29ad640ef598cddeedbbd054936623
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 21:17:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array <<
+  array << "kuya"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.shift
 end
 
 def remove_element_from_end_of_array(array)
